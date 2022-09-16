2dbb5849869aae3a1fd909c72557e7530451abd8
wakataw <pratamaagung@hotmail.com>
2022-09-16 16:17:57 +0700

    implement promiseOutput function untuk filter jumlah film berdasarkan emosi

--- promise.js
+++ promise.js
@@ -1,7 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  // fetch data dari promise iix dan vgc
+  const moviesIxx = await promiseTheaterIXX();
+  const moviesVgc = await promiseTheaterVGC();
+
+  // combine iix & vgc, filter berdasarkan emosi, lalu hitung jumlah member
+  return moviesIxx
+    .concat(moviesVgc)
+    .filter((i) => i.hasil === emosi)
+    .length;
+};
 
 module.exports = {
   promiseOutput,
